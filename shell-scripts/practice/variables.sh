a567f94548362dca457d88078bec6186484a85e7
Asik-KM <knytz1504@gmail.com>
2024-09-05 17:16:18 +0530

    variables

--- shell-scripts/practice/variables.sh
+++ shell-scripts/practice/variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 person1=$1
 person2=$2
-echo "$person1: Hi $person2!"
-echo "$person2: Hello $person1. How are you?"
-echo "$person1: I am doing good $person2"
-echo "$person2: Activate command mode"
+echo "$R $person1: Hi $person2! $R"
+echo "$G $person2: Hello $person1. How are you? $G"
+echo "$B $person1: I am doing good $person2 $B"
+echo "$person2: $G Activate command mode"
