b7d05905a29d3e42cd40802856023e1a3dd9b61f
Asik-KM <knytz1504@gmail.com>
2024-09-05 17:24:23 +0530

    variables

--- shell-scripts/practice/variables.sh
+++ shell-scripts/practice/variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 person1=$1
 person2=$2
+R="\e[31m"
+G="\e[32m"
+B="\e[0m"
 echo "$R $person1: Hi $person2! $R"
 echo "$G $person2: Hello $person1. How are you? $G"
 echo "$B $person1: I am doing good $person2 $B"
