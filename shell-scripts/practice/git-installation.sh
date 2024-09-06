216b34caa6903d9af2d508d2baaf74231e3385a1
Asik-KM <knytz1504@gmail.com>
2024-09-06 15:53:18 +0530

    Installing git

--- shell-scripts/practice/git-installation.sh
+++ shell-scripts/practice/git-installation.sh
@@ -4,7 +4,20 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then
     echo "Switch to root" 
-else 
+    exit 1
+fi   
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
     dnf install git -y
-    echo "Bye bye"
+    if [ $? -ne 0 ]
+    then 
+        echo "git not installed"
+    else   
+        echo "git installed"
+    fi
+else
+    echo "git already installed"
 fi
