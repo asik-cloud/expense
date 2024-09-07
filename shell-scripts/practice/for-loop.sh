3b0583bf8542267e20b1c683c8b309d9c0b66685
Asik-KM <knytz1504@gmail.com>
2024-09-07 22:22:53 +0530

    updates

---
+++ shell-scripts/practice/for-loop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+ROOT_CHECK(){
+    if ( $USER -ne 0)
+    then    
+        echo "Switch to root user"
+        exit 1
+    fi
+}
+
+ROOT_CHECK
+
+
+dnf list installed git
+if ( $? -ne 0 )
+then
+    echo "installing git"
+    dnf install git -y
+else
+    echo "Git already installed"
+fi
