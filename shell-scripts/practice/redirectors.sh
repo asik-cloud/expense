61f66bdbef772d31d2fd0e3863823781f5640410
Asik-KM <knytz1504@gmail.com>
2024-09-07 15:04:19 +0530

    updates

--- shell-scripts/practice/redirectors.sh
+++ shell-scripts/practice/redirectors.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 USER=$(id -u)
+LOG_FOLDER=$(/var/log/Shellscripts)
+SCRIT_NAME=$(echo $0 | cut -d\ "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIT_NAME-$TIMESTAMP.log"
+mkdir -p $LOG_FOLDER
+
 
 ROOT_CHECK(){
     if [ $USER -ne 0 ]
     then
-        echo "swich to Root"
+        echo "swich to Root" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -14,9 +20,9 @@ VALIDATE(){
  
     if [ $1 -ne 0 ]
     then
-        echo "$2 failed"
+        echo "$2 failed" | tee -a $LOG_FILE
     else
-        echo "$2 Success"
+        echo "$2 Success" | tee -a $LOG_FILE
     fi
 }
 
@@ -38,13 +44,13 @@ ROOT_CHECK
 
 for package in $@ # $@ refers to all arguments passed to it
 do
-    dnf list installed $package 
+    dnf list installed $package | tee -a $LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it.." 
-        dnf install $package -y 
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo "$package is already installed..nothing to do" 
+        echo "$package is already installed..nothing to do" | tee -a $LOG_FILE
     fi
 done
