379196b76cb4296de4e3da21706808afa9cc7f15
Asik-KM <knytz1504@gmail.com>
2024-09-08 17:04:29 +0530

    backend

--- shell-scripts/Expense/backend.sh
+++ shell-scripts/Expense/backend.sh
@@ -1,97 +1,85 @@
 #!/bin/bash
-USER=$(id -u)
-LOG_FOLDER="/var/log/expense"
-SCRIT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%y-%m-%d_%h%M%s)
-LOG_FILE=$LOG_FOLDER/$SCRIT_NAME-$TIMESTAMP.log
-sudo mkdir -p $LOG_FOLDER
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
-ROOT_CHECK(){
-
-    if [ $USER -ne 0 ]
-    then  
-        echo "swich to root"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
 VALIDATE(){
-
     if [ $1 -ne 0 ]
     then
-        echo -e "$2...$R failed $N" | tee -a $LOG_FILE
-    else    
-        echo -e "$2...$G success $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
-
 }
 
-ROOT_CHECK
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling Nodesjs 16"
+VALIDATE $? "Disable default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Nodesjs:20"
+VALIDATE $? "Enable nodejs:20"
 
-dnf list installed nodejs &>>$LOG_FILE
-    if [ $? -ne 0 ]
-    then    
-        echo "Installing NodeJs"
-        dnf install nodejs -y &>>$LOG_FILE
-        VALIDATE $? "Installing NodeJs"
-    else
-        echo "NodeJs already Installed"
-    fi
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
 
 id expense &>>$LOG_FILE
-    if [ $? -ne 0 ]
-    then    
-        echo "Adding User expense"
-        useradd expense&>>$LOG_FILE
-        VALIDATE $? "Adding user"
-    else
-        echo "User Already exists."
-    fi
- mkdir -p /app &>>$LOG_FILE
- curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
- VALIDATE $? "Downloading backend data"
-
- mkdir -p /app
- cd /app
- rm -rf /app/*
- unzip /tmp/backend.zip &>>$LOG_FILE
- VALIDATE $? "Extracting the data"
-
-npm install &>>$LOG_FILE
-
-cp /home/ec2-user/practice-81s/shell-scripts/Expense/backend.service /etc/systemd/system/backend.service
-
-systemctl daemon-reload 
-
-systemctl enable backend
-VALIDATE $? "Enabling"
-
-systemctl start backend
-VALIDATE $? "Starting/restarting"
-
-yum list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
-then 
-    echo " Installing mysql client"
-    dnf install mysql -y &>>$LOG_FILE
-    VALIDATE $? "Installing mysql client"
+then
+    echo -e "expense user not exists... $G Creating $N"
+    useradd expense &>>$LOG_FILE
+    VALIDATE $? "Creating expense user"
 else
-    echo " Already installed ...skipping"
+    echo -e "expense user already exists...$Y SKIPPING $N"
 fi
 
-mysql -h 18.215.176.75 -uroot -pExpenseApp@1 < /app/schema/backend.sql | tee -a $LOG_FILE
-VALIDATE $? "Loading Schema"
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
 
-systemctl restart backend
-VALIDATE $? "Restarting backend"
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend application code"
 
+cd /app
+rm -rf /app/* # remove the existing code
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/practice-81s/shell-scripts/Expensebackend.service /etc/systemd/system/backend.service
+
+# load the data before running backend
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h 18.215.176.75 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restarted Backend"
--- shell-scripts/Expense/mysql.sh
+++ shell-scripts/Expense/mysql.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
-USER=$(id -u)
-LOG_FOLDER="/var/log/expense"
-SCRIT_NAME=$(echo $0 | cut -d "." -f1)
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOG_FOLDER/$SCRIT_NAME-$TIMESTAMP.log"
-mkdir -p $LOG_FOLDER
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+Y="\e[33m"
 
-ROOT_CHECK(){
-
-    if [ $USER -ne 0 ]
-    then 
-        echo "Switch to root"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
-VALIDATE(){
 
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e " $2...$G failed $N "
-
-    else    
-        echo -e " $2...$R success $N "
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-ROOT_CHECK
-dnf list installed mysql-server | tee -a $LOG_FILE
-if [ $? -ne 0 ]
-then 
-    echo "Installing mysql server"
-    dnf install mysql-server -y &>>$LOG_FILE
-    VALIDATE $? "Installing mysql-server"
-else
-    echo "mysql server is already installed"
-fi
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabling mysql server"
+VALIDATE $? "Enabled MySQL Server"
 
 systemctl start mysqld &>>$LOG_FILE
-VALIDATE $? "Starting mysql server"
+VALIDATE $? "Started MySQL server"
 
 mysql -h 18.215.176.75 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "setting up password now"
-    mysql_secure_installation --set-root-pass ExpenseApp@1 | tee -a $LOG_FILE
-    VALIDATE $? "Password set"
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
 else
-    echo "Password already setup..Skipping"
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
 fi
+
