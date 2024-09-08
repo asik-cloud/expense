e5e1256b4159163c8dfa16188fc3f1c2d0682110
Asik-KM <knytz1504@gmail.com>
2024-09-08 16:10:01 +0530

    backend

--- shell-scripts/Expense/backend.sh
+++ shell-scripts/Expense/backend.sh
@@ -4,7 +4,7 @@ LOG_FOLDER="/var/log/expense"
 SCRIT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%y-%m-%d_%h%M%s)
 LOG_FILE=$LOG_FOLDER/$SCRIT_NAME.log
-mkdir -p $LOG_FOLDER
+sudo mkdir -p $LOG_FOLDER
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
